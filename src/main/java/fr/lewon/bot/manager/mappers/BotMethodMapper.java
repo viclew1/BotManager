4debff964a6287e25dcf7381ced128b051b46bbf
Victor Lewon <viclew@gmail.com>
2019-11-16 12:25:30 +0100

    Update to follow DefaultBot's update

--- src/main/java/fr/lewon/bot/manager/mappers/BotMethodMapper.java
+++ src/main/java/fr/lewon/bot/manager/mappers/BotMethodMapper.java
@@ -1,31 +1,30 @@
 package fr.lewon.bot.manager.mappers;
 
-import java.util.List;
-
+import fr.lewon.bot.manager.entities.BotMethodDTO;
+import fr.lewon.bot.manager.entities.BotPropertyDescriptorDTO;
+import fr.lewon.bot.methods.AbstractBotMethodProcessor;
+import fr.lewon.bot.runner.BotRunner;
 import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
-import fr.lewon.bot.manager.entities.BotMethodDTO;
-import fr.lewon.bot.manager.entities.BotPropertyDescriptorDTO;
-import fr.lewon.bot.methods.AbstractBotMethodProcessor;
-import fr.lewon.bot.runner.BotRunner;
+import java.util.List;
 
 @Mapper(
-		componentModel = "spring",
-		uses = { BotPropertyMapper.class })
+        componentModel = "spring",
+        uses = {BotPropertyMapper.class})
 public interface BotMethodMapper {
 
-	public List<BotMethodDTO> botMethodToDto(List<AbstractBotMethodProcessor<?, ?>> botMethod, @Context BotRunner runner);
+    List<BotMethodDTO> botMethodToDto(List<? extends AbstractBotMethodProcessor> botMethod, @Context BotRunner runner);
 
-	@Mapping(target = "params", source = "botMethod", qualifiedByName = "fetchNeededParams")
-	public BotMethodDTO botMethodToDto(AbstractBotMethodProcessor<?, ?> botMethod, @Context BotRunner runner);
+    @Mapping(target = "params", source = "botMethod", qualifiedByName = "fetchNeededParams")
+    BotMethodDTO botMethodToDto(AbstractBotMethodProcessor botMethod, @Context BotRunner runner);
 
-	@Named("fetchNeededParams")
-	default List<BotPropertyDescriptorDTO> fetchNeededParams(AbstractBotMethodProcessor<?, ?> botMethod, @Context BotRunner runner) {
-		return Mappers.getMapper(BotPropertyMapper.class).botPropertiesToDto(botMethod.getNeededParameters(runner));
-	}
+    @Named("fetchNeededParams")
+    default List<BotPropertyDescriptorDTO> fetchNeededParams(AbstractBotMethodProcessor botMethod, @Context BotRunner runner) {
+        return Mappers.getMapper(BotPropertyMapper.class).botPropertiesToDto(botMethod.getNeededParameters(runner));
+    }
 
 }
--- src/main/java/fr/lewon/bot/manager/modele/BotRunnersManager.java
+++ src/main/java/fr/lewon/bot/manager/modele/BotRunnersManager.java
@@ -1,10 +1,5 @@
 package fr.lewon.bot.manager.modele;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import fr.lewon.bot.errors.BotRunnerException;
 import fr.lewon.bot.errors.WrongStateRunnerException;
 import fr.lewon.bot.manager.util.BotRunnerBuilderFactory;
@@ -15,89 +10,98 @@ import fr.lewon.bot.manager.util.errors.NoBotFoundException;
 import fr.lewon.bot.runner.BotRunner;
 import fr.lewon.bot.runner.BotState;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
 public enum BotRunnersManager {
 
-	INSTANCE;
+    INSTANCE;
 
-	private final List<GameInfos> gameInfos;
+    private final List<GameInfos> gameInfos;
 
-	private BotRunnersManager() {
-		gameInfos = Arrays.asList(BotRunnerBuilderFactory.values()).stream()
-				.map(b -> new GameInfos(b.name(), b.getGameName()))
-				.collect(Collectors.toList());
-	}
 
-	public void createBot(String login, String password, Map<String, Object> params, BotRunnerBuilderFactory bf) throws BotManagerException, BotRunnerException {
-		GameInfos gi = findGameInfosById(bf.name());
-		verifyNotRunning(gi, login);
-		BotRunner runner = bf.getBotRunnerBuilder().buildRunner(login, password);
-		runner.init(params);
-		gi.getRunnerInfos().add(new RunnerInfos(runner, login));
-	}
+    BotRunnersManager() {
+        this
 
-	public RunnerInfos getRunnerInfos(String gameId, Long id) throws BotManagerException {
-		return getRunnerInfos(findGameInfosById(gameId), id);
-	}
-	
-	public RunnerInfos getRunnerInfos(GameInfos gameInfos, Long id) throws BotManagerException {
-		return gameInfos.getRunnerInfos().stream()
-				.filter(r -> r.getId().equals(id))
-				.findFirst()
-				.orElseThrow(() -> new NoBotFoundException(id));
-	}
+                .gameInfos = (List) Arrays.asList(BotRunnerBuilderFactory.values()).stream().map(b -> new GameInfos(b.name(), b.getGameName())).collect(Collectors.toList());
+    }
 
-	public void stopBot(String gameId, Long id) throws BotManagerException, BotRunnerException {
-		RunnerInfos runnerInfos = getRunnerInfos(gameId, id);
-		runnerInfos.getBotRunner().stop();
-	}
 
-	public void pauseBot(String gameId, Long id) throws BotManagerException, BotRunnerException {
-		RunnerInfos runnerInfos = getRunnerInfos(gameId, id);
-		runnerInfos.getBotRunner().togglePause();
-	}
+    public void createBot(String login, String password, Map<String, Object> params, BotRunnerBuilderFactory bf) throws BotManagerException, BotRunnerException {
+        GameInfos gi = this.findGameInfosById(bf.name());
+        this.verifyNotRunning(gi, login);
+        BotRunner<?> runner = bf.getBotRunnerBuilder().buildRunner(login, password, params);
+        gi.getRunnerInfos().add(new RunnerInfos(runner, login));
+    }
 
-	public void startBot(String gameId, Long id) throws BotManagerException, BotRunnerException {
-		RunnerInfos runnerInfos = getRunnerInfos(gameId, id);
-		runnerInfos.getBotRunner().start();
-	}
 
-	private void verifyNotRunning(GameInfos gameInfos, String login) throws AlreadyRunningBotException {
-		for (RunnerInfos bi : gameInfos.getRunnerInfos()) {
-			if (bi.getLogin().equals(login)) {
-				throw new AlreadyRunningBotException(gameInfos.getName(), login);
-			}
-		}
-	}
+    public RunnerInfos getRunnerInfos(String gameId, Long id) throws BotManagerException {
+        return this.getRunnerInfos(this.findGameInfosById(gameId), id);
+    }
 
-	public GameInfos findGameInfosById(String gameId) throws NoBotForThisGameException {
-		return gameInfos.stream()
-				.filter(g -> g.getId().equals(gameId))
-				.findFirst()
-				.orElseThrow(() -> new NoBotForThisGameException(gameId));
-	}
 
-	public void trimStoppedBots() throws BotManagerException, BotRunnerException {
-		for (GameInfos gi : gameInfos) {
-			for (RunnerInfos ri : gi.getRunnerInfos()) {
-				if (ri.getBotRunner().getState() == BotState.STOPPED) {
-					trimStoppedBot(gi.getId(), ri.getId());
-				}
-			}
-		}
-	}
+    public RunnerInfos getRunnerInfos(GameInfos gameInfos, Long id) throws BotManagerException {
+        return (RunnerInfos) gameInfos.getRunnerInfos().stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new NoBotFoundException(id));
+    }
 
-	public void trimStoppedBot(String gameId, Long id) throws BotManagerException, BotRunnerException {
-		GameInfos gameInfos = findGameInfosById(gameId);
-		RunnerInfos runnerInfos = getRunnerInfos(gameId, id);
-		if (runnerInfos.getBotRunner().getState() != BotState.STOPPED) {
-			throw new WrongStateRunnerException("Trim", runnerInfos.getBotRunner().getState(), BotState.STOPPED);
-		}
-		runnerInfos.getBotRunner().kill();
-		gameInfos.getRunnerInfos().remove(runnerInfos);
-	}
+    public void stopBot(String gameId, Long id) throws BotManagerException, BotRunnerException {
+        RunnerInfos runnerInfos = this.getRunnerInfos(gameId, id);
+        runnerInfos.getBotRunner().stop();
+    }
 
-	public List<GameInfos> getGameInfos() {
-		return gameInfos;
-	}
+    public void pauseBot(String gameId, Long id) throws BotManagerException, BotRunnerException {
+        RunnerInfos runnerInfos = this.getRunnerInfos(gameId, id);
+        runnerInfos.getBotRunner().togglePause();
+    }
 
+    public void startBot(String gameId, Long id) throws BotManagerException, BotRunnerException {
+        RunnerInfos runnerInfos = this.getRunnerInfos(gameId, id);
+        runnerInfos.getBotRunner().start();
+    }
+
+    private void verifyNotRunning(GameInfos gameInfos, String login) throws AlreadyRunningBotException {
+        for (RunnerInfos bi : gameInfos.getRunnerInfos()) {
+            if (bi.getLogin().equals(login)) {
+                throw new AlreadyRunningBotException(gameInfos.getName(), login);
+            }
+        }
+    }
+
+    public GameInfos findGameInfosById(String gameId) throws NoBotForThisGameException {
+        return (GameInfos) this.gameInfos.stream()
+                .filter(g -> g.getId().equals(gameId))
+                .findFirst()
+                .orElseThrow(() -> new NoBotForThisGameException(gameId));
+    }
+
+    public void trimStoppedBots() throws BotRunnerException, BotManagerException {
+        for (GameInfos gi : this.gameInfos) {
+            for (RunnerInfos ri : gi.getRunnerInfos()) {
+                if (ri.getBotRunner().getState() == BotState.STOPPED) {
+                    this.trimStoppedBot(gi.getId(), ri.getId());
+                }
+            }
+        }
+    }
+
+    public void trimStoppedBot(String gameId, Long id) throws BotManagerException, BotRunnerException {
+        GameInfos gameInfos = this.findGameInfosById(gameId);
+        RunnerInfos runnerInfos = this.getRunnerInfos(gameId, id);
+        if (runnerInfos.getBotRunner().getState() != BotState.STOPPED) {
+            throw new WrongStateRunnerException("Trim", runnerInfos.getBotRunner().getState(), BotState.STOPPED);
+        }
+        runnerInfos.getBotRunner().kill();
+        gameInfos.getRunnerInfos().remove(runnerInfos);
+    }
+
+
+    public List<GameInfos> getGameInfos() {
+        return this.gameInfos;
+    }
 }
